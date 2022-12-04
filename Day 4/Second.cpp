44d4b678fa4b5efebc031ece7f39099bc640bf8c
Wilhelm Jansson <34298694+WilleMahMille@users.noreply.github.com>
2022-12-04 14:19:47 +0900

    Day 4 added

---
+++ Day 4/Second.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+
+bool overlapping(int oneS, int oneE, int twoS, int twoE) {
+	if (oneS >= twoS && oneS <= twoE) {
+		return true;
+	}
+	if (oneE >= twoS && oneE <= twoE) {
+		return true;
+	}
+	if (twoS >= oneS && twoS <= oneE) {
+		return true;
+	}
+	if (twoE >= oneS && twoE <= oneE) {
+		return true;
+	}
+	return false;
+}
+
+int main() {
+	fstream f;
+	f.open("input.txt");
+	string line = "";
+	int sum = 0;
+	while (!f.eof()) {
+		getline(f, line);
+		int dash = line.find('-');
+		int comma = line.find(',');
+		int oneStart = stoi(line.substr(0, dash));
+		int oneEnd = stoi(line.substr(dash + 1, comma));
+		string partTwo = line.substr(comma + 1, line.length());
+		dash = partTwo.find('-');
+		int twoStart = stoi(partTwo.substr(0, dash));
+		int twoEnd = stoi(partTwo.substr(dash + 1, partTwo.length()));
+		sum += overlapping(oneStart, oneEnd, twoStart, twoEnd);
+
+	}
+	std::cout << sum << "\n";
+}
+
+
