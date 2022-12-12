e6cc13711ea2661366234d9a0d5a7dbc1e707fff
Wilhelm Jansson <34298694+WilleMahMille@users.noreply.github.com>
2022-12-12 09:41:38 +0900

    Day 11
    I have 2 solutions this time, 1 for the boring string manipulation and 1 for the hard-coded monkeys which I did out of spite of having to deal with strings :)

---
+++ Day 11/First Restructured.cpp
@@ -0,0 +1,126 @@
+/*
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+
+long long int monkeyOP(long long int old, int monkey) {
+	switch (monkey) {
+	case 0:
+		return old * 19;
+	case 1:
+		return old * 11;
+	case 2:
+		return old + 6;
+	case 3:
+		return old + 5;
+	case 4:
+		return old + 7;
+	case 5:
+		return old * old;
+	case 6:
+		return old + 2;
+	case 7:
+		return old + 3;
+	default:
+		std::cout << "incorrect monkey\n";
+		return 0;
+	}
+}
+
+int monkeyTE(long long int item, int monkey) {
+	switch (monkey) {
+	case 0:
+		return item % 17 == 0 ? 4 : 7;
+	case 1:
+		return item % 3 == 0 ? 3 : 2;
+	case 2:
+		return item % 19 == 0 ? 0 : 4;
+	case 3:
+		return item % 7 == 0 ? 2 : 0;
+	case 4:
+		return item % 2 == 0 ? 7 : 5;
+	case 5:
+		return item % 5 == 0 ? 1 : 6;
+	case 6:
+		return item % 11 == 0 ? 3 : 1;
+	case 7:
+		return item % 13 == 0 ? 5 : 6;
+	default:
+		std::cout << "incorrect monkey\n";
+		return -1;
+	}
+}
+
+vector<vector<long long int>> items = { {72, 64, 51, 57, 93, 97, 68}, {62},
+		{57, 94, 69, 79, 72}, {80, 64, 92, 93, 64, 56}, {70, 88, 95, 99, 78, 72, 65, 94},
+		{57, 95, 81, 61}, {79, 99}, {68, 98, 62} };
+
+/*
+vector<vector<int>> items = { {79, 98}, {54, 65, 75, 74}, {79, 60, 97}, {74} };
+long long int monkeyOP(long long int old, int monkey) {
+	switch (monkey) {
+	case 0:
+		return old * 19;
+	case 1:
+		return old + 6;
+	case 2:
+		return old * old;
+	case 3:
+		return old + 3;
+	default:
+		std::cout << "incorrect monkey\n";
+		return 0;
+	}
+}
+int monkeyTE(int item, int monkey) {
+	switch (monkey) {
+	case 0:
+		return item % 23 == 0 ? 2 : 3;
+	case 1:
+		return item % 19 == 0 ? 2 : 0;
+	case 2:
+		return item % 13 == 0 ? 1 : 3;
+	case 3:
+		return item % 17 == 0 ? 0 : 1;
+	default:
+		std::cout << "incorrect monkey\n";
+		return -1;
+	}
+}
+*/
+/*
+
+int main() {
+	// i dont care anymore :)
+	
+	
+	int itemInspections[8] = { 0 };
+	for (int i = 0; i < 10'000; i++) {
+		for (int j = 0; j < 8; j++) {
+			while (items[j].size() > 0) {
+				long long int item = monkeyOP(items[j][0], j);
+				item = item % (17 * 3 * 19 * 7 * 2 * 5 * 11 * 13); //9'699'690;
+				int to = monkeyTE(item, j);
+				itemInspections[j]++;
+				if (item < 0) {
+					std::cout << "overflowed\n";
+				}
+				items[to].push_back(item);
+				items[j].erase(items[j].begin());
+			}
+		}
+	}
+
+	for (int i = 0; i < 8; i++) {
+		std::cout << itemInspections[i] << "\n";
+	}
+	std::cout << "done\n";
+
+}
+
+*/
---
+++ Day 11/First.cpp
@@ -0,0 +1,163 @@
+
+/*
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+
+class Monkey {
+private:
+	char operation;
+	int var;
+	long long int divNum;
+	vector<long long int> items;
+	int falseMonkey;
+	int trueMonkey;
+	int monkeyNum;
+	int itemsInspected = 0;
+public:
+	Monkey(vector<long long int> items, char operation, int var, int divNum, int falseMonkey, int trueMonkey) {
+		this->items = items;
+		this->operation = operation;
+		this->var = var;
+		this->divNum = divNum;
+		this->falseMonkey = falseMonkey;
+		this->trueMonkey = trueMonkey;
+	}
+
+	void operate() {
+		itemsInspected++;
+		long long int temp = this->var == -1 ? items[0] : this->var;
+		switch (this->operation)
+		{
+		case 'A':
+			items[0] += temp;
+			break;
+		case 'M':
+			items[0] *= temp;
+			break;
+		default:
+			std::cout << "error\n";
+			break;
+		}
+	}
+	int test() {
+		this->operate();
+		items[0] /= 3;
+		if (items[0] % this->divNum == 0) {
+			return this->trueMonkey;
+		}
+		return this->falseMonkey;
+	}
+	long long int itemThrow() {
+		long long int temp = items[0];
+		this->items.erase(this->items.begin());
+		return temp;
+	}
+	bool hasRound() {
+		return this->items.size() != 0;
+	}
+	int getInspected() {
+		return itemsInspected;
+	}
+	void receiveItem(long long int item) {
+		items.push_back(item);
+	}
+
+};
+
+vector<long long int> parseItems(string items) {
+	int start = 0;
+	vector<long long int> itemVec;
+	for (int i = 0; i < items.length(); i++) {
+		if (items[i] == ',') {
+			itemVec.push_back(stoi(items.substr(start, i - start)));
+			start = i + 1;
+		}
+	}
+	itemVec.push_back(stoi(items.substr(start, items.length())));
+	return itemVec;
+}
+
+Monkey parseMonkey(vector<string> line) {
+	vector<long long int> items = parseItems(line[1].substr(17, line[1].length()));
+	char op = line[2][23];
+	switch (op) {
+	case '*':
+		op = 'M';
+		break;
+	case '+':
+		op = 'A';
+		break;
+	default:
+		std::cout << "wrong operator\n";
+		break;
+	}
+	string strVar = line[2].substr(25, line[2].length());
+	int var = -1;
+	if (strVar != "old") {
+		var = stoi(strVar);
+	}
+	int divNum = stoi(line[3].substr(21, line[3].length()));
+	int trueMonkey = stoi(line[4].substr(line[4].find('y') + 1, line[4].length()));
+	int falseMonkey = stoi(line[5].substr(line[5].find('y') + 1, line[5].length()));
+
+	Monkey monkey = Monkey(items, op, var, divNum, falseMonkey, trueMonkey);
+	return monkey;
+}
+
+int main() {
+	fstream f;
+	f.open("input.txt");
+	string line = "";
+	vector<Monkey> monkeys;
+
+	while (!f.eof()) {
+		vector<string> temp = vector<string>();
+		getline(f, line);
+		while (!f.eof() && line != "") {
+			temp.push_back(line);
+			getline(f, line);
+		}
+		temp.push_back(line);
+		monkeys.push_back(parseMonkey(temp));
+	}
+	std::cout << "loaded monkeys\n";
+
+	for (int i = 0; i < 20; i++) {
+		for (int j = 0; j < monkeys.size(); j++) {
+			while (monkeys[j].hasRound()) {
+				int thrownTo = monkeys[j].test();
+				long long int item = monkeys[j].itemThrow();
+				if (item < 0) {
+					std::cout << "overflowed\n";
+				}
+				monkeys[thrownTo].receiveItem(item);
+			}
+		}
+	}
+
+	int max[2] = { 0 };
+	int index[2] = { 0 };
+	for (int i = 0; i < monkeys.size(); i++) {
+		if (monkeys[i].getInspected() > max[0]) {
+			max[1] = max[0];
+			index[1] = index[0];
+			max[0] = monkeys[i].getInspected();
+			index[0] = i;
+		}
+		else if (monkeys[i].getInspected() > max[1]) {
+			max[1] = monkeys[i].getInspected();
+			index[1] = i;
+		}
+	}
+	std::cout << max[0] * max[1] << "\n";
+
+}
+
+
+*/
